37d520d0acead06a5a81577c8e7aba8df7c55c3a
Glenn Hogan <89474054+glennhogan@users.noreply.github.com>
2023-10-16 13:33:47 -0400

    Black and white test data set finalized

--- SRC/Color_to_bw_convert.py
+++ SRC/Color_to_bw_convert.py
@@ -1,3 +1,23 @@
+import os
 from PIL import Image
-img = Image.open('image.png').convert('L')
-img.save('greyscale.png')
+
+# Define the folder path
+folder_path = '..\\DATA\\black_and_white_test_set\\'
+
+# Loop through each file in the folder
+for file_name in os.listdir(folder_path):
+    # Check if the file is a JPG
+    if file_name.endswith('.JPEG'):
+        #print("if statement passes")
+        # Create a full file path
+        file_path = os.path.join(folder_path, file_name)
+        
+        # Load the image
+        with Image.open(file_path) as img:
+            #convert to black and white using pillow
+            img_gray = img.convert('L')
+
+            # Save the rotated image (overwriting the original or to a new location)
+            img_gray.save(os.path.join(folder_path, 'bw_' + file_name))
+            #removing old images
+        os.remove(file_path)
