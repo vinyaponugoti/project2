91da4793cdf260df9e9d1ef530b078809f9ef38b
Justin Brady <jmb4zz@virginia.edu>
2023-10-22 11:51:18 -0400

    adding captions and trainimages files for random contrast dataset

--- SRC/test.py
+++ SRC/test.py
@@ -1,4 +1,4 @@
-filename = "../DATA/flicker30k-captions.txt"
+filename = "../DATA/Flickr_30k.trainImages.txt"
 
 def load_doc(filename):
     # Opening the file as read only
@@ -11,16 +11,26 @@ def load_doc(filename):
 def all_img_captions(filename):
     file = load_doc(filename)
     captions = file.split('\n')
-    descriptions ={}
-    trainfile = open("../DATA/Flickr_30k.trainImages.txt", 'w')
-    for caption in captions[:-1]:
-        #print(caption)
-        img, caption = caption.split(', ', 1)
-        if img not in descriptions:
-            descriptions[img] = [ caption ]
-            trainfile.write(img + "\n")
-        else:
-            descriptions[img].append(caption)
-    return descriptions
 
-dictionary = all_img_captions(filename)
+    new_captions_file = open("../DATA/flickr30k-random-contrast-trainImages.txt", "w")
+
+    for caption in captions[:-1]:
+        name, extension = caption.split(".")
+        name = name + "_random_contrast"
+        extension = "jpeg"
+        new_filename = name + "." + extension
+
+        new_captions_file.write(new_filename + "\n")
+    # descriptions ={}
+    # trainfile = open("../DATA/Flickr_30k.trainImages.txt", 'w')
+    # for caption in captions[:-1]:
+    #     print(caption)
+    #     img, caption = caption.split(', ', 1)
+    #     if img not in descriptions:
+    #         descriptions[img] = [ caption ]
+    #         trainfile.write(img + "\n")
+    #     else:
+    #         descriptions[img].append(caption)
+    # return descriptions
+
+all_img_captions(filename)
